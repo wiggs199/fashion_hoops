cd280d700ff5968720a2915b212a1782be5f4b81
Norm Wiggins <wiggs199@gmail.com>
2021-07-28 00:25:25 -0400

    Create database & begin model packaging

--- blog.py
+++ blog.py
@@ -1,10 +1,11 @@
-from datetime import datetime
+
 from logging import debug
 from typing import DefaultDict
 from flask import Flask,render_template,url_for,flash,redirect
 from wtforms.validators import Email
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm,LoginForm
+from models import User,Posts
 
 app = Flask(__name__)
 # Set Secret key to protect against cookies when using forms 
@@ -13,28 +14,6 @@ app.config['SECRET_KEY'] = '9307f719f3ac67ac0b2e25193fb03f93'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 # Database instance 
 db = SQLAlchemy(app)
-
-# Create Columns
-class User(db.Model) : 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email= db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20),  nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
-    post = db.relationship('Post',backref= 'author', lazy=True)
-
-    # MAgic Method used to print object
-    def __repr__(self) :
-        return f"User('{self.username}','{self.email}','{self.image_file}')"
-
-class Post(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
-    user_id = db.Column(db.Integer,db.ForeignKey('user.id'), nullable=False)
-
-    def __repr__(self) :
-        return f"Post('{self.title}','{self.date_posted}')"
 
 
 
---
+++ models.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from blog import db
+from models import User,Posts
+
+
+# Create Columns
+class User(db.Model) : 
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email= db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20),  nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    post = db.relationship('Post',backref= 'author', lazy=True)
+
+    # MAgic Method used to print object
+    def __repr__(self) :
+        return f"User('{self.username}','{self.email}','{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self) :
+        return f"Post('{self.title}','{self.date_posted}')"
+
+
+
